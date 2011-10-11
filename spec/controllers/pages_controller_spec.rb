cbdaac880e0a9ffa96933fd26831135788494f0d
Charadzey <charadzey@gmail.com>
2011-10-11 15:57:38 +0300

    Task 1 chapter 3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -47,4 +47,21 @@ describe PagesController do
 
   end
 
+  describe "GET 'help'" do
+
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | Help")
+    end
+
+  end
+
+
 end
